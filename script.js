edb2ce218d3be31f8e277ee2acc92692466d5f7f
JuneXll <71652307+JuneXll@users.noreply.github.com>
2021-03-22 16:35:17 -0400

    Finished function for password generator

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
-// // Assignment Code
-// var generateBtn = document.querySelector("#generate");
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-//   passwordText.value = password;
+  passwordText.value = password;
 
-// }
+}
 
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
-
-
-// Ask length of password=> at least 8-128 characters
-var userLength = 0;
-var validLength = true;
-for (var i=0; validLength; i++){
+// Character randomization
+// Add character randomization to password
+function generatePassword() {
+  // Ask length of password=> at least 8-128 characters
+  var userLength = 0;
+  var validLength = true;
+  
+  for (var i=0; validLength; i++){
   var userLength = prompt("Choose a password length between 8 and 128 characters.")
   if((userLength < 8) || (userLength > 128)){
   alert("Your password is not between the right parameters, please choose a length between 8 and 128 characters.");
@@ -29,71 +31,71 @@ for (var i=0; validLength; i++){
   } else {
     break;
   }
-} 
+  } 
 
-console.log(userLength);
+ console.log(userLength);
 
-// Ask character types=>lowercase, uppercase, numeric and/or special characters
-var lowerCase = confirm("Would you like to have lowercase letters in your password?");
-console.log(lowerCase);
-var upperCase = confirm("Would you like to have uppercase letters in your password?");
-console.log(upperCase);
-var numbers = confirm("Would you like to have numbers in your password?");
-console.log(numbers);
-var specialChar = confirm("Would you like the following special characters in your password? (!@#$%^&*)");
-console.log(specialChar);
+ // Ask character types=>lowercase, uppercase, numeric and/or special characters
+ var lowerCase = confirm("Would you like to have lowercase letters in your password?");
+ console.log(lowerCase);
+ var upperCase = confirm("Would you like to have uppercase letters in your password?");
+ console.log(upperCase);
+ var numbers = confirm("Would you like to have numbers in your password?");
+ console.log(numbers);
+ var specialChar = confirm("Would you like the following special characters in your password? (!@#$%^&*)");
+ console.log(specialChar);
 
-// Create random password
-var alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];//uppercase
-var numeric = [0,1,2,3,4,5,6,7,8,9];
-var special = ["!","@","#","$","%","^","&","*"];
+  // Create random password
+  var alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];//uppercase
+  var numeric = [0,1,2,3,4,5,6,7,8,9];
+  var special = ["!","@","#","$","%","^","&","*"];
 
-function generateRandomNumber(userLength){
+  function generateRandomNumber(userLength){
   var randomNumber = Math.random();
   var result = Math.floor(randomNumber*userLength);
   return result;
-}
+  }
 
-// Character randomization
-var randomAlpha = generateRandomNumber(alpha.length);
-var randomLetter = alpha[randomAlpha];
-console.log(randomLetter);
-
-var randomCapAlpha = generateRandomNumber(alpha.length);
-var randomCapLetter = alpha[randomCapAlpha].toUpperCase();
-console.log(randomCapLetter);
-
-var randomNum = generateRandomNumber(numeric.length);
-var randomNumberPW = numeric[randomNum];
-console.log(randomNumberPW);
-
-var randomSpecial = generateRandomNumber(special.length);
-var randomSpecialPW = special[randomSpecial];
-console.log(randomSpecialPW);
-
-// Add character randomization to password
-
-// function generatePassword() {
   var passwordArray =[];
-  while(passwordArray.length < userLength){
+  
+  for (var j=0; j < userLength;j++){
+    var randomAlpha = generateRandomNumber(alpha.length);
+    var randomLetter = alpha[randomAlpha];
+    console.log(randomLetter);
+
+    var randomCapAlpha = generateRandomNumber(alpha.length);
+    var randomCapLetter = alpha[randomCapAlpha].toUpperCase();
+    console.log(randomCapLetter);
+
+    var randomNum = generateRandomNumber(numeric.length);
+    var randomNumberPW = numeric[randomNum];
+    console.log(randomNumberPW);
+
+    var randomSpecial = generateRandomNumber(special.length);
+    var randomSpecialPW = special[randomSpecial];
+    console.log(randomSpecialPW);
     if (lowerCase===true){
       passwordArray.push(randomLetter);
     } 
-    if(upperCase===true){
+    if (upperCase===true){
       passwordArray.push(randomCapLetter);
     }
     if (numbers===true){
       passwordArray.push(randomNumberPW);
     }
-    if(specialChar===true){
+    if (specialChar===true){
       passwordArray.push(randomSpecialPW);
     }
-    console.log(passwordArray);
-    i++;
+    if (passwordArray.length===userLength){
+      break;
+    }
   }
-// }
+  passwordArray=passwordArray.splice((userLength-1),(userLength*1));
+  passwordArray= passwordArray.join('');
+  return passwordArray;
+}
 
-// console.log(generatePassword());
+console.log(generatePassword());
 
 // Password is generated and shows on page
 
